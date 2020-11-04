419d697cc5278403218da049a0af0ab9a3e261d0
pavi poorani <pavipoorani1999@outlook.com>
2020-11-04 17:10:41 +0330

    exception

---
+++ CITS.Exception/LoginException.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CITS.Exception
+{
+    class LoginException:ApplicationException
+    {
+        public LoginException() : base()
+        {
+
+        }
+        public LoginException(string message) : base(message)
+        {
+
+        }
+
+    }
+}
+
